cd20aa14f7b49699ee3d0b7c909eeded0b1f9f6a
Azim Momin <ayyem@protonmail.com>
2024-04-16 13:30:35 -0400

    Added meteor rotation and scaling.

--- game/meteor.py
+++ game/meteor.py
@@ -10,20 +10,37 @@ class Meteor(pygame.sprite.Sprite):
         super().__init__(groups)
 
         # Todo: Optimize this. We load this image each time we spawn a laser.
-        self.image = pygame.image.load("data/graphics/meteor.png").convert_alpha()
+        meteor_surface: pygame.Surface = pygame.image.load(
+            "data/graphics/meteor.png"
+        ).convert_alpha()
+        scale_factor: float = uniform(0.5, 1.5)
+        self.original_image: pygame.Surface = pygame.transform.scale_by(
+            meteor_surface, (scale_factor, scale_factor)
+        )
+        self.image: pygame.Surface = self.original_image
 
         x_pos: float = randint(-100, WINDOW_WIDTH + 100)
         y_pos: float = randint(-150, -50)
-        self.rect = self.image.get_rect(center=(x_pos, y_pos))
-        self.pos = self.rect.topleft
+        self.rect: pygame.Rect = self.image.get_rect(center=(x_pos, y_pos))
+        self.pos: pygame.Vector2 = self.rect.topleft
 
         lower, upper = METEOR_SPEED_RANGE
         self.speed = randint(lower, upper)
         self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
 
+        self.rotation = 0
+        self.rotation_speed = randint(20, 50)
+
+    def rotate(self, deltaTime: float):
+        self.rotation += self.rotation_speed * deltaTime
+        rotated_surface = pygame.transform.rotozoom(self.original_image, self.rotation)
+        self.image = rotated_surface
+        self.rect = pygame.image.get_rect(center=self.rect.center)
+
     def update(self, deltaTime):
         self.pos += self.direction * self.speed * deltaTime
         self.rect.topleft = (round(self.pos.x), round(self.pos.y))
+        self.rotate(deltaTime=deltaTime)
 
     def is_on_screen(self) -> bool:
         return self.rect.y < WINDOW_HEIGHT
